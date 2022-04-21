30d0f7eb6d2cee73407b9017167960dcf5252d9a
RWHarrison <hamiltonwayne34@gmail.com>
2022-04-21 10:19:49 +0100

    first commit

--- script.js
+++ script.js
@@ -25,7 +25,7 @@ function tellMe(joke) {
 // Get jokes from Joke API
 async function getJokes() {
   let joke = '';
-  const apiUrl = 'https://sv443.net/jokeapi/v2/joke/Programming?blacklistFlags=nsfw,racist,sexist';
+  const apiUrl = 'https://v2.jokeapi.dev/joke/Any';
   try {
     const response = await fetch(apiUrl);
     const data = await response.json();
